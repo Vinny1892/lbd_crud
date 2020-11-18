6643e2abceb10004fe4cf4e47598987e91353d9f
Vinicius Espindola <vinnyaoe@gmail.com>
2020-11-18 03:29:37 -0400

    started dependent feature

---
+++ resources/views/dependente/dependente_form.blade.php
@@ -0,0 +1,42 @@
+@extends('layout.index')
+@section('content')
+@if (session('message'))
+    <div class="alert alert-success">
+        {{ session('message') }}
+    </div>
+@endif
+@if ($errors->any())
+    @foreach ($errors->all() as $error)
+        <div> <p>{{ $error }}</p> </div>
+    @endforeach
+@endif
+    @if(isset($dependente))
+    <form method="POST" action="{{ route('dependente.update', ["dependente" => $dependente->id]) }}" >
+        @method("PUT")
+    @else
+        <form method="POST" action="{{ route('dependente.store') }}">
+    @endif
+        @csrf
+        <input type="text" name="nome"  placeholder="Digite o nome do funcionário"
+               value="{{ $funcionario ? $funcionario->nome : old('nome') }}">
+        <input type="text" name="cpf"  placeholder="Digite o cpf do funcionário"
+               value="{{ $funcionario ? $funcionario->cpf : old('cpf') }}">
+        <input type="text" name="endereco"  placeholder="Digite o endereco do funcionário"
+               value="{{ $funcionario ? $funcionario->endereco : old('endereco') }}">
+        <select name="setor">
+            <option value="" ></option>
+            @foreach($setores as $setor)
+                <option value="{{ $setor->id }}"
+
+                    @if(isset($funcionario) && $funcionario->setor()->exists())
+                    {{ $funcionario->setor->nome === $setor->nome ? 'selected' : ''  }}
+                    @endif
+                >
+                    {{$setor->nome}}
+                </option>
+            @endforeach
+        </select>
+        <button type="submit" >Submit</button>
+    </form>
+@endsection
+
